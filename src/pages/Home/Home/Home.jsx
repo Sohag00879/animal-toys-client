ae50f41f333b4f0a7eea0d9c8823fe9d9f5780dd
sohag838 <95273824+sohag838@users.noreply.github.com>
2023-05-18 23:59:23 +0600

    added remote control toys section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import RemoteControlToy from "../RemoteControlToy/RemoteControlToy";
 import SeaAnimalsTube from "../sea-amimails-tube/SeaAnimalsTube";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <RemoteControlToy></RemoteControlToy>
       <SeaAnimalsTube></SeaAnimalsTube>
     </div>
   );
---
+++ src/pages/Home/RemoteControlToy/RemoteControlToy.jsx
@@ -0,0 +1,61 @@
+const RemoteControlToy = () => {
+  return (
+    <div className="mt-6">
+      <h2 className="text-3xl font-bold text-center mb-6">
+        REMOTE CONTROL TOYS
+      </h2>
+      <div className="grid grid-cols-3 gap-14">
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://terrabybattat.com/wp-content/uploads/AN2801_PR_MAIN-768x768.png"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Emerald Tree Boa</h2>
+            <p>Watch out for those creepy-crawlies!</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://terrabybattat.com/wp-content/uploads/AN2818_PR_MAIN-768x768.png"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Blue Tarantula</h2>
+            <p>Use your remote control to guide your animal toys</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://terrabybattat.com/wp-content/uploads/AN2854_PR_MAIN-768x768.png"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Giant Scolopendra</h2>
+            <p>
+              They slither and scurry through the forests and deserts of the
+              world.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RemoteControlToy;
