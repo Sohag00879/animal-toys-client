795fa3a3f0a6e5fff1762d7cf652b0cc973bb00c
sohag838 <95273824+sohag838@users.noreply.github.com>
2023-05-21 19:20:17 +0600

    search button added

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,21 +1,79 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ShowAllToys from "./ShowAllToys";
 
 const AllToys = () => {
+  const [storedToys, setStoredToys] = useState([]);
   const [allToys, setAllToys] = useState([]);
+  const searchRef = useRef();
+  const [isSearch, setIsSearch] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:5000/allToys")
       .then((res) => res.json())
       .then((data) => {
         setAllToys(data);
+        setStoredToys(data);
         console.log(data);
+        setIsSearch(false);
       });
   }, []);
+
+  const handleSearch = () => {
+    setIsSearch(true);
+    const text = searchRef.current.value.toLowerCase();
+    // filter the value according to text
+    const filterdValue = storedToys.filter((toy) =>
+      toy?.name?.toLowerCase()?.includes(text)
+    );
+    setAllToys(filterdValue);
+    console.log(filterdValue);
+  };
+  const handleRemoveSearch = () => {
+    setIsSearch(false);
+    setAllToys(storedToys);
+    searchRef.current.value = "";
+  };
 
   return (
     <div className="mt-16 mb-16">
       <h2 className="font-bold text-center text-3xl mb-14">ALL TOYS</h2>
+      {/* serach */}
+      <div className="form-control">
+        <div className="input-group">
+          <div className="relative">
+            <input
+              ref={searchRef}
+              type="text"
+              placeholder="Search…"
+              className="input input-bordered"
+            />
+            {isSearch && (
+              <p
+                onClick={handleRemoveSearch}
+                className="absolute top-1/4 right-2 cursor-pointer"
+              >
+                X
+              </p>
+            )}
+          </div>
+          <button onClick={handleSearch} className="btn btn-square">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
       <div className="overflow-x-auto w-full">
         <table className="table w-full">
           {/* head */}
--- src/pages/Home/ToyTab/TabToy.jsx
+++ src/pages/Home/ToyTab/TabToy.jsx
@@ -1,25 +1,141 @@
-import React from "react";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
 const TabToy = () => {
   return (
-    <div>
+    <div className="mt-12 mb-12">
+      <h2 className="font-bold text-center mt-12 mb-12 text-3xl">
+        DIFFERNT CATEGORIES TOY
+      </h2>
       <Tabs>
         <TabList>
-          <Tab>Title 1</Tab>
-          <Tab>Title 2</Tab>
-          <Tab>Title 3</Tab>
+          <Tab>Wild</Tab>
+          <Tab>Fish</Tab>
+          <Tab>Bird</Tab>
         </TabList>
 
+        {/* Wild */}
         <TabPanel>
-          <h2>Any content 1</h2>
+          <div className="grid grid-cols-2">
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src="https://terrabybattat.com/wp-content/uploads/AN2712-pr-c-768x768.jpg"
+                  alt="Quarter Horse"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Quarter Horse</h2>
+                <p>
+                  This realistic Quarter Horse toy makes a great gift and an
+                  ideal goodie bag stuffer addition for an animal-themed party.
+                </p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src="https://terrabybattat.com/wp-content/uploads/AN2697_PR-768x768.png"
+                  alt="Gorilla"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Gorilla</h2>
+                <p>
+                  This realistic gorilla makes a great gift and an ideal goodie
+                  bag stuffer addition for an animal-themed party.
+                </p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </TabPanel>
+        {/* Fish */}
         <TabPanel>
-          <h2>Any content 2</h2>
+          <div className="grid grid-cols-2">
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src="https://terrabybattat.com/wp-content/uploads/AN2611-pr-c-hs-768x768.jpg"
+                  alt="Dolphin"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Dolphin</h2>
+                <p>
+                  The dolphin is a favorite among children and adults alike.{" "}
+                </p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src="https://terrabybattat.com/wp-content/uploads/AN2604-dp-A-hs-768x768.jpg"
+                  alt="Great White Shark"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Great White Shark</h2>
+                <p>
+                  The great white shark is a favorite among children and adults
+                  alike.
+                </p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </TabPanel>
+        {/* Bird */}
         <TabPanel>
-          <h2>Any content 3</h2>
+          <div className="grid grid-cols-2">
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src="https://terrabybattat.com/wp-content/uploads/AN2621_PR-768x768.png"
+                  alt="Bald Eagle"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Bald Eagle</h2>
+                <p>
+                  The bald eagle is a majestic bird found in North America. This
+                  bald eagle toy makes a great gift and an ideal goodie bag
+                  stuffer addition for an animal-themed party.
+                </p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src="https://terrabybattat.com/wp-content/uploads/AN2767_PR-768x768.jpg"
+                  alt="Emperor Penguin & Baby"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Emperor Penguin & Baby</h2>
+                <p>
+                  This realistic penguin toy set makes a great gift and an ideal
+                  goodie bag stuffer addition for an animal-themed party.
+                </p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </TabPanel>
       </Tabs>
     </div>
